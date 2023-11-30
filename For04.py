4947ca1c7294da155739a4eac05332b43a80aaac
samandar <samandar.abduhakimov.007@gmail.com>
2023-12-01 02:55:47 +0500

    nb

--- For04.py
+++ For04.py
@@ -1,10 +1,18 @@
-def main(A,B):
+def find_max_index(data):
     """
-    Return the numbers from A to B in the form of a list.
-    Args:
-        A: int
-        B: int
-    Returns:
-        list: return  answer
+    Given the list of numbers, return the index of maximum number in the list
+    args:
+        data: list of numbers
+    returns: index of maximum number in the list
     """
-    return
+    i = 0
+    mx = data[0]
+    while i < len(data):
+        x = data[i]
+        if x > mx:
+            mx = x
+        i += 1
+
+    return data.index(mx)
+print(find_max_index([1, 2, 3, 4, 5]))
+print(find_max_index([6, 8, 7, 4, 0]))
