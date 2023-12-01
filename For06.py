d3b2d65ed6584abd9cd716a7c4e3869927153458
samandar <samandar.abduhakimov.007@gmail.com>
2023-12-01 10:32:48 +0500

    d

--- For06.py
+++ For06.py
@@ -1,20 +1,10 @@
-def find_max_odd(data):
+def main(A,B):
     """
-    Given the list of numbers, Find the maximum odd number in the list
-    args:
-        data: list of numbers
-    returns: maximum odd number in the list
+    Return the sum of all integers from A to B.
+    Args:
+        A: int
+        B: int
+    Returns:
+        int: return  answer
     """
-    i=0
-    mx=data[0]
-    while i<len(data):
-        x=data[i]
-        if x > mx:
-            mx=x
-        i+=1
-    return mx
-
-
-
-print(find_max_odd([1, 8, 3, 8, 5]))
-print(find_max_odd([11, 7, 5, 4, 9]))
+    return
